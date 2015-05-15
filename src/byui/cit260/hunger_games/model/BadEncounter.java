15e4559af551d6b48c01fc6a5f06aa0866dbb38a
NguyenSonny <ngu12001@byui.edu>
2015-05-15 10:45:35 -0600

    05/15/15 - Added remaining classes

---
+++ src/byui/cit260/hunger_games/model/BadEncounter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hunger_games.model;
+
+/**
+ *
+ * @author WINGU
+ */
+public class BadEncounter {
+    
+}
