85acf71dfe6ecb14754a6a9e9f7bd017b86cc9d2
NguyenSonny <ngu12001@byui.edu>
2015-05-16 19:46:09 -0600

    05/15/15 - Edited Tributes

--- src/hunger_games_text/Hunger_games_text.java
+++ src/hunger_games_text/Hunger_games_text.java
@@ -8,6 +8,9 @@ package hunger_games_text;
 import byui.cit260.hunger_games.model.Inventory;
 import byui.cit260.hunger_games.model.Item;
 import byui.cit260.hunger_games.model.Location;
+import byui.cit260.hunger_games.model.Map;
+import byui.cit260.hunger_games.model.Player;
+import byui.cit260.hunger_games.model.Scene;
 import byui.cit260.hunger_games.model.Tributes;
 
 /**
@@ -19,46 +22,66 @@ import byui.cit260.hunger_games.model.Tributes;
 public class Hunger_games_text {
 
     public static void main(String[] args){
+        //Player
+       Player created = new Player();
+       created.getName();
+       
+        
+        
         //Tribute one Male
     Tributes oneM = new Tributes();
-        oneM.setName("jett");
+        oneM.setName("Jett");
+        oneM.setGender("male");
+        oneM.setDistrict("District 1");
+        oneM.setSkills("poison daggers");
         oneM.setHealthPoints(150);
-        oneM.setSkills("dagger");
-        oneM.setStrength(5);
+        oneM.setStrength(20);
 
         String TributesIOneM = oneM.toString();
         System.out.println(oneM);
         
          //Tribute one female
      Tributes oneF = new Tributes();
-        oneF.setName("molly");
-        oneF.setHealthPoints(45);
-        oneF.setSkills("sword");
-        oneF.setStrength(5);
+        oneF.setName("Scarlett");
+        oneF.setGender("female");
+        oneF.setDistrict("District 1");
+        oneF.setSkills("whip");
+        oneF.setHealthPoints(150);
+        oneF.setStrength(20);
 
         String TributesOneF = oneF.toString();
         System.out.println(oneF);
         
        //Tribute two Male
      Tributes twoM = new Tributes();
-        twoM.setName("Steve");
-        twoM.setHealthPoints(45);
-        twoM.setSkills("sword");
-        twoM.setStrength(5);
+        twoM.setName("Mako");
+        twoM.setGender("male");
+        twoM.setDistrict("District 2");
+        twoM.setSkills("iron rod");
+        twoM.setHealthPoints(100);
+        twoM.setStrength(15);
         
         String TributesTwoM = twoM.toString();
         System.out.println(twoM);
         
          //Tribute two female
      Tributes twoF = new Tributes();
-        twoF.setName("Allie");
-        twoF.setHealthPoints(45);
-        twoF.setSkills("sword");
-        twoF.setStrength(5);
+        twoM.setName("Angel");
+        twoM.setGender("");
+        twoM.setDistrict("District 2");
+        twoM.setSkills("brick");
+        twoM.setHealthPoints(95);
+        twoM.setStrength(15);
 
         String TributesTwoF = twoF.toString();
         System.out.println(twoF);
          
+        //MAP
+        Map area = new Map();
+        area.setRow(1);
+        area.setColumn(2);
+       
+        
         //LOCATION
         Location place = new Location();
         place.setRow("1");
@@ -91,7 +114,7 @@ public class Hunger_games_text {
         inventory[2] = new Item("Blue Berry", 7);
         inventory[3] = new Item("Pear", 7);
         inventory[4] = new Item("Fish", 12);
-        inventory[5] = new Item("Pumpkin", 2);
+        inventory[5] = new Item("Herb", 2);
         
         
         for (int i = 0; i < inventory.length; i++) {
