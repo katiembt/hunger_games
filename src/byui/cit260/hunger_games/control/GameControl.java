143fb38e5996254f103ae2d7bc1c78aa276f7956
NguyenSonny <ngu12001@byui.edu>
2015-05-27 11:05:22 -0600

    05/27/15 - Added controls

--- src/byui/cit260/hunger_games/control/GameControl.java
+++ src/byui/cit260/hunger_games/control/GameControl.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.cit260.hunger_games.model;
+package byui.cit260.hunger_games.control;
 
 /**
  *
  * @author WINGU
  */
-public class Aid {
-        public static void main (String [] args){
-        
- 
+public class GameControl {
+    
+}
