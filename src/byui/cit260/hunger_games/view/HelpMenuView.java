0820082130b1b92264b275c74a2d602d4dd3ad96
katie tooley <katherineblake94@gmail.com>
2015-06-10 22:06:16 -0600

    05/14/15 - Initial commit to repository 05/16/15 - commit of Location, Item, and Inventory 05/16/15 - commit of hunger_games 06/1/15 - commit battlecalc 06/9/15 - re start team :( 06/9/15 - fixed a lot of the program 06/10/15 - process on team assignment 06/10/15 - more work on mainMenuView 06/10/15 - added views

---
+++ src/byui/cit260/hunger_games/view/HelpMenuView.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hunger_games.view;
+
+/**
+ *
+ * @author katherineblake
+ */
+public class HelpMenuView {
+             switch (choice){
+            case 'G':
+                this.helpGamePurpose();
+                break;
+             case 'M':
+                this.helpMove();
+                break;
+            case 'B':
+                this.helpBattle();
+                break;
+            case 'S':
+                this.helpHarvest();
+                break; 
+            case 'S':
+                this.helpFood();
+                break;
+            case 'Q':
+                return;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try Again");
+                break;
+                
+        }
+}
