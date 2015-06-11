df7397a0209851c325190134f98abe10b4183a21
katie tooley <katherineblake94@gmail.com>
2015-06-10 22:59:24 -0600

    05/14/15 - Initial commit to repository 05/16/15 - commit of Location, Item, and Inventory 05/16/15 - commit of hunger_games 06/1/15 - commit battlecalc 06/9/15 - re start team :( 06/9/15 - fixed a lot of the program 06/10/15 - process on team assignment 06/10/15 - more work on mainMenuView 06/10/15 - added views 6/10/15 - control

--- src/byui/cit260/hunger_games/view/MapControlView.java
+++ src/byui/cit260/hunger_games/view/MapControlView.java
@@ -5,10 +5,88 @@
  */
 package byui.cit260.hunger_games.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author katherineblake
  */
 public class MapControlView {
+    private final String MENU ="\n"
+            + "\n----------------------------------------"
+            + "\n| Map Main Menu                    |"
+            + "\n----------------------------------------"
+            + "\nM - Move"
+            + "\nH - Go to Harvast"
+            + "\nE - Exit"
+            + "\n----------------------------------------";
+            
     
+    void displayMenu(){
+        
+        char selection = ' ';
+        do { 
+        
+        System.out.println(MENU); //display Menu
+        
+        String input = this.getInput(); // get user selection
+        selection = input.charAt(0); //getfirst character of string
+        
+        this.doAction(selection);     //do action based on selction
+        
+        } while (selection != 'E'); //an sel is not "EXIT"
+    }
+
+    private String getInput() {
+        boolean valid = false; 
+        String getInput = null;
+        Scanner keyboard = new Scanner(System.in);
+        
+        while(!valid){
+            
+            System.out.println("Enter The letter of your Selection Below:");
+            
+           getInput = keyboard.nextLine();
+           getInput = getInput.trim();
+            
+            if(getInput.length() < 1){
+                System.out.println("Invalid Input - the Input must not be blank");
+                continue;
+                
+            }
+            break;
+        }
+        return getInput;
+        
+        
+    }
+
+    private void doAction(char choice) {
+        
+        switch (choice){
+            case 'M':
+                this.displayMoveMenu();
+                break; 
+            case 'H':
+                this.displayHarvestMenu();
+                break;     
+            case 'E':
+                return;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try Again");
+                break;
+                
+        }
+    }
+
+    private void displayHarvestMenu() {
+         System.out.println("*** displayHarvestMenu function called***");
+    }
+
+    private void displayMoveMenu() {
+         System.out.println("*** displayMoveMenu function called***");
+    }
+
+   
+     
 }
