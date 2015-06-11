f515cb61816a78e41bd9760cb958d7ac7087cb9e
NguyenSonny <ngu12001@byui.edu>
2015-06-10 23:55:12 -0600

    06/10/15 11:30PM - AID AND BATTLE VIEWS

--- src/byui/cit260/hunger_games/view/harvestMenuView.java
+++ src/byui/cit260/hunger_games/view/harvestMenuView.java
@@ -5,10 +5,79 @@
  */
 package byui.cit260.hunger_games.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author katherineblake
  */
 public class harvestMenuView {
+       
+    private final String MENU ="\n"
+            + "\n----------------------------------------"
+            + "\n| Harvest Main Menu                    |"
+            + "\n----------------------------------------"
+            + "\nH - Harvest"
+            + "\nE - Exit Harvest"
+            + "\n----------------------------------------";
+            
     
+    void displayMenu(){
+        
+        char selection = ' ';
+        do { 
+        
+        System.out.println(MENU); //display Menu
+        
+        String input = this.getInput(); // get user selection
+        selection = input.charAt(0); //getfirst character of string
+        
+        this.doAction(selection);     //do action based on selction
+        
+        } while (selection != 'E'); //an sel is not "EXIT"
+    }
+
+    private String getInput() {
+        boolean valid = false; 
+        String getInput = null;
+        Scanner keyboard = new Scanner(System.in);
+        
+        while(!valid){
+            
+            System.out.println("Enter The letter of your Selection Below:");
+            
+           getInput = keyboard.nextLine();
+           getInput = getInput.trim();
+            
+            if(getInput.length() < 1){
+                System.out.println("Invalid Input - the Input must not be blank");
+                continue;
+                
+            }
+            break;
+        }
+        return getInput;
+        
+        
+    }
+
+    private void doAction(char choice) {
+        
+        switch (choice){
+            case 'H':
+                this.displayHarvestMenu();
+                break; 
+            case 'E':
+                return;
+            default:
+                System.out.println("\n*** Invalid Selection *** Try Again");
+                break;
+                
+        }
+    }
+
+    private void displayHarvestMenu() {
+        System.out.println("*** displayHarvestMenu function called***");
+    }
+     
 }
