7590d6a6a774d144738c3f18c5f4be8287c06b4b
katie tooley <katherineblake94@gmail.com>
2015-07-03 20:09:49 -0600

    7/3/15 Individual Assignment (Harvest Control Exception)

---
+++ src/exceptions/HarvestControlException.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author katherineblake
+ */
+public class HarvestControlException {
+
+    public HarvestControlException() {
+    }
+    public HarvestControlException(String message) {
+        super();
+    }
+
+    public HarvestControlException(String message, Throwable cause) {
+        super();
+    }
+
+    public HarvestControlException(Throwable cause) {
+        super();
+    }
+
+    public HarvestControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super();
+    }
+}
