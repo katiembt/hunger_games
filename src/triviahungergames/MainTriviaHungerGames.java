5e1354196ee83628d044f83cf3d190a27af67c31
katie tooley <katherineblake94@gmail.com>
2015-07-08 11:16:25 -0600

    7/8/15 team assignement

--- src/triviahungergames/MainTriviaHungerGames.java
+++ src/triviahungergames/MainTriviaHungerGames.java
@@ -5,6 +5,12 @@
  */
 package triviahungergames;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import triviahungergames.model.Game;
 import triviahungergames.model.Location;
 import triviahungergames.model.Map;
@@ -19,15 +25,57 @@ public class MainTriviaHungerGames {
     private static Game currentGame = null;
     private static Player player = null;
 
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        MainTriviaHungerGames.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        MainTriviaHungerGames.inFile = inFile;
+    }
+    
      public static void main(String[] args) {
         
-        StartProgramView startProgramView = new StartProgramView();
+//        StartProgramView startProgramView = new StartProgramView();
         try {
-          startProgramView.startProgram();   
+            
+            //open character stream files for end user input and output
+            MainTriviaHungerGames.inFile = 
+                    new BufferedReader(new InputStreamReader(System.in));
+            
+            MainTriviaHungerGames.outFile = 
+                    new PrintWriter(System.out, true);            
+        
+           StartProgramView startProgramView = new StartProgramView();
+            startProgramView.startProgram();   
         } catch (Throwable te) {
+            
             System.out.println(te.getMessage());
             te.printStackTrace();
             startProgramView.display();
+        }
+        finally{
+            try {
+                if(MainTriviaHungerGames.inFile !=null)
+                   MainTriviaHungerGames.inFile.close();
+                
+                if(MainTriviaHungerGames.outFile !=null)
+                   MainTriviaHungerGames.outFile.close();
+            } catch (IOException ex) {
+               System.out.println("error closing files");
+               return;
+            }
+            
         }
         public static Game getCurrentGame(){
         return currentGame;
